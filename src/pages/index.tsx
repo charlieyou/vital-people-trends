2418cf46d438ce476469c268b2d0f9d85d1d1384
Charlie You <charlie@rwa.xyz>
2024-01-20 13:11:26 -0500

    Add footer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import Head from 'next/head';
 import Image from 'next/image';
 import Link from 'next/link';
 
@@ -16,24 +17,43 @@ const Home = () => {
   }, []);
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-white text-black text-center">
-      <p className="text-2xl mb-2">There are</p>
-      {peopleCount !== null ? (
-        <p className="text-9xl font-bold mb-2">{peopleCount}</p>
-      ) : (
-        <p className="text-9xl font-bold mb-2">...</p>
-      )}
-      <p className="text-2xl mb-4">people at</p>
-      <Link href="https://www.vitalclimbinggym.com/brooklyn" passHref>
-          <Image 
-            src="/vital.png" 
-            alt="Vital" 
-            width={300} 
-            height={120}
-            className="inline-block"
-          />
-      </Link>
-    </div>
+    <>
+      <Head>
+        <title>Vital People Count</title>
+        <link 
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" 
+          rel="stylesheet"
+        />
+      </Head>
+      <div className="flex flex-col min-h-screen">
+          <main className="flex flex-col items-center justify-center flex-grow bg-white text-black text-center">
+            <p className="text-2xl mb-2">There are</p>
+            {peopleCount !== null ? (
+              <p className="text-9xl font-bold mb-2">{peopleCount}</p>
+            ) : (
+              <p className="text-9xl font-bold mb-2">...</p>
+            )}
+            <p className="text-2xl mb-4">people at</p>
+            <Link href="https://www.vitalclimbinggym.com/brooklyn" passHref>
+                <Image 
+                  src="/vital.png" 
+                  alt="Vital" 
+                  width={300} 
+                  height={120}
+                  className="inline-block"
+                />
+            </Link>
+        </main>
+        <footer className="text-center p-4">
+          <p className="text-lg">
+            Built with <span className="text-red-500">❤️</span> by Charlie
+            <Link href="https://instagram.com/cyouclimbs">
+              <i className="fab fa-instagram ml-2 cursor-pointer"></i>
+            </Link>
+          </p>
+        </footer>
+      </div>
+    </>
   );
 };
 
